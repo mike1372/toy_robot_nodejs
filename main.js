66caec832cd25f65ec0f84fc6f7d06888da1aba2
Michael Peter Kempster <MichaelKempster@Michaels-MacBook-Pro.local>
2019-08-18 16:28:11 +1000

    Started the interpreter module.

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-console.log('Starting simulation');
+let interpreters = require('./interpreter.js');
+let Interpreter = interpreters.Interpreter;
 
 var standard_input = process.stdin;
-
 standard_input.setEncoding('utf-8');
+
+// Instantiate all objects required for the simulation
+let interpreter = new Interpreter;
+
+console.log('Starting simulation');
 
 // Main loop
 standard_input.on('data', function (data) {
-    if(data === 'exit\n') {
+    if (data === 'exit\n') {
         console.log('Simulation exiting');
         process.exit(0);
     } else {
-        console.log('Command is ' + data);
+        interpreter.process(data);
     }
 });
