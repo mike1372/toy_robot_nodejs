b4af52288a86498d7ee5b5837b21873e8361c05a
Michael Peter Kempster <MichaelKempster@Michaels-MacBook-Pro.local>
2019-08-19 11:34:12 +1000

    Move board instance outside of test.

--- tests/board.js
+++ tests/board.js
@@ -4,8 +4,9 @@ let boards = require('../lib/board.js');
 let Board = boards.Board;
 
 describe('Board', function() {
+  var board = new Board;
+
   it('The size of the board should be 5 by 5', function() {
-    var board = new Board;
     expect(board.size()).to.equal('This board is of length 5 and width 5');
   });
 });
