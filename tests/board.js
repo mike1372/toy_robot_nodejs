6688d9621a292bcefdff560db5e4ae6c8db1b3ae
Michael Peter Kempster <MichaelKempster@Michaels-MacBook-Pro.local>
2019-08-19 11:01:39 +1000

    Reorganise directory structure.

--- tests/board.js
+++ tests/board.js
@@ -1,10 +1,10 @@
 var chai = require('chai');
 var expect = chai.expect;
-let boards = require('../board.js');
+let boards = require('../lib/board.js');
 let Board = boards.Board;
 
 describe('Board', function() {
-  it('something', function() {
+  it('The size of the board should be 5 by 5', function() {
     var board = new Board;
     expect(board.size()).to.equal('This board is of length 5 and width 5');
   });
